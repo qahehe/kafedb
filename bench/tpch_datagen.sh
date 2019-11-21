ce9101db9915a66b57773582d8755231bf5ceb64
Zheguang Zhao <zheguang.zhao@gmail.com>
2019-11-20 23:38:11 -0700

    minor: bnech/tpch_datagen remove reps

--- bench/tpch_datagen.sh
+++ bench/tpch_datagen.sh
@@ -7,22 +7,15 @@ if [[ $# < 2 ]]; then
 	  exit 123
 fi
 
-echo "dex variant=$1"
-echo "reps=$2"
-
 jar_jdbc=third_party/postgresql-42.0.0.jar
 jar_bc=third_party/bcprov-jdk15on-164.jar
 
-for i in $(eval echo {1..$reps}); do
-	  echo "rep=$i"
-	  ./bin/spark-submit --master local[*] \
-			                 --jars $jar_jdbc,$jar_bc \
-			                 --driver-class-path $jar_jdbc:$jar_bc  \
-			                 --conf spark.executor.extraClassPath=$jar_jdbc:$jar_bc \
-			                 --driver-memory 6g   \
-			                 --conf spark.driver.maxResultSize=0 \
-			                 --class org.apache.spark.examples.sql.dex.TPCHDataGen ./examples/target/scala-2.11/jars/spark-examples_2.11-2.4.0.jar \
-			                 $@
-done
-
+./bin/spark-submit --master local[*] \
+			             --jars $jar_jdbc,$jar_bc \
+			             --driver-class-path $jar_jdbc:$jar_bc  \
+			             --conf spark.executor.extraClassPath=$jar_jdbc:$jar_bc \
+			             --driver-memory 6g   \
+			             --conf spark.driver.maxResultSize=0 \
+			             --class org.apache.spark.examples.sql.dex.TPCHDataGen ./examples/target/scala-2.11/jars/spark-examples_2.11-2.4.0.jar \
+			             $@
 echo "done"
