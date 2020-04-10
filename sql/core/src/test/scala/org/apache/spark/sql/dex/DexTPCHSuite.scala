8d89af38acc584a8d1ff028d2ef3e49229478746
Zheguang Zhao <zheguang.zhao@gmail.com>
2020-04-09 20:20:29 -0400

    pkfk: enable tests

--- sql/core/src/test/scala/org/apache/spark/sql/dex/DexTPCHSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/dex/DexTPCHSuite.scala
@@ -40,42 +40,42 @@ class DexTPCHSuite extends DexPkfkTPCHTest {
   }
 
   test("one pk-fk join one filter on pk table") {
-    val query = supplier.join(partsupp).where("s_suppkey == ps_suppkey and s_name == 'sa'")
+    val query = supplier.join(partsupp).where("s_suppkey == ps_suppkey and s_name == 'sa'").select("s_name")
     checkDexFor(query, query.dexPkFk(pks, fks))
   }
 
   test("one pk-fk join one filter on fk table") {
-    val query = supplier.join(partsupp).where("s_suppkey == ps_suppkey and ps_comment == 'psb'")
+    val query = supplier.join(partsupp).where("s_suppkey == ps_suppkey and ps_comment == 'psb'").select("s_name")
     checkDexFor(query, query.dexPkFk(pks, fks))
   }
 
   test("one fk-pk join one filter on pk table") {
-    val query = partsupp.join(supplier).where("ps_suppkey == s_suppkey and s_name == 'sa'")
+    val query = partsupp.join(supplier).where("ps_suppkey == s_suppkey and s_name == 'sa'").select("ps_comment")
     checkDexFor(query, query.dexPkFk(pks, fks))
   }
 
   test("one fk-pk join one filter on fk table") {
-    val query = partsupp.join(supplier).where("ps_suppkey == s_suppkey and ps_comment == 'psb'")
+    val query = partsupp.join(supplier).where("ps_suppkey == s_suppkey and ps_comment == 'psb'").select("s_name")
     checkDexFor(query, query.dexPkFk(pks, fks))
   }
 
   test("two joins: fk-pk and fk-pk") {
-    val query = partsupp.join(supplier).where("ps_suppkey == s_suppkey").join(part).where("ps_partkey == p_partkey")
+    val query = partsupp.join(supplier).where("ps_suppkey == s_suppkey").join(part).where("ps_partkey == p_partkey").select("s_name")
     checkDexFor(query, query.dexPkFk(pks, fks))
   }
 
   test("two joins: pk-fk and pk-fk") {
-    val query = supplier.join(part.join(partsupp).where("p_partkey == ps_partkey")).where("s_suppkey == ps_suppkey")
+    val query = supplier.join(part.join(partsupp).where("p_partkey == ps_partkey")).where("s_suppkey == ps_suppkey").select("s_name")
     checkDexFor(query, query.dexPkFk(pks, fks))
   }
 
   test("two joins: fk-pk and pk-fk") {
-    val query = part.join(partsupp.join(supplier).where("ps_suppkey == s_suppkey")).where("p_partkey == ps_partkey")
+    val query = part.join(partsupp.join(supplier).where("ps_suppkey == s_suppkey")).where("p_partkey == ps_partkey").select("s_name")
     checkDexFor(query, query.dexPkFk(pks, fks))
   }
 
   test("two joins: pk-fk and fk-pk") {
-    val query = part.join(partsupp).where("p_partkey == ps_partkey").join(supplier).where("ps_suppkey == s_suppkey")
+    val query = part.join(partsupp).where("p_partkey == ps_partkey").join(supplier).where("ps_suppkey == s_suppkey").select("s_name")
     checkDexFor(query, query.dexPkFk(pks, fks))
   }
 
